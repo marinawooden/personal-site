44146191988ada1cb35428700bc152ac3b0a3930
marinawooden <mwoode@uw.edu>
2022-09-30 08:51:05 -0700

    added some styling and changed some fonts

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 "use strict";
 (function() {
+  const OFFSET = 2;
+  const NUM_LET = 4;
+  const SPEED = 2;
+  
   window.addEventListener("load", init);
 
   function init() {
@@ -7,6 +11,33 @@
   }
 
   function wiggle(text) {
+    text.forEach((e, i) => {
+      const words = e.textContent;
+
+      text[i].classList.add("wiggle");
+      text[i].innerHTML = "";
+
+      for (let j = 0; j < words.length; j++) {
+        const sp = document.createElement("span");
+        sp.textContent = words[j];
+        // Maybe some day these letters will be colorful...
+        // sp.style.color = "hsl(" + Math.floor(Math.random() * 361) + ", 100%, 50%)";
+
+        text[i].appendChild(sp);
+      }
+
+      let loop = 0;
+      setInterval(() => {
+        for (let j = 0; j < words.length; j++) {
+          // Nobody knows how this works- not even me and I wrote the equation :-(
+          const magicNumber = NUM_LET * OFFSET;
+          const yCoord = ((magicNumber) + (OFFSET * j) + (OFFSET * loop)) % (magicNumber);
+
+          text[i].children[j].style.top = yCoord + "px";
+        }
+        loop++;
+      }, SPEED * 100);
+    });
   }
 
   function qsa(query) {
